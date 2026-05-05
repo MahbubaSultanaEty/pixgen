de6ee70e9b8e900a31abeab75638506960ecc818
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-05-05 18:13:03 +0600

    update user data , name and image

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { UpdateUserModal } from "@/components/UpdateUserModal";
 import { authClient } from "@/lib/auth-client";
 import { Avatar, Card } from "@heroui/react";
 import React from "react";
@@ -6,7 +7,7 @@ import React from "react";
 const ProfilePage = () => {
   const userData = authClient.useSession();
   const user = userData.data?.user;
-  console.log(user);
+//   console.log(user);
   return (
     <div className="my-12">
       <Card className="max-w-8/12 mx-auto flex flex-col items-center">
@@ -18,7 +19,9 @@ const ProfilePage = () => {
                   <Avatar.Fallback>{ user?.name.charAt(0)}</Avatar.Fallback>
               </Avatar>
               <h2 className="text-xl font-bold text-blue-900">{user?.name}</h2>
-              <p className="text-xs font-semibold text-zinc-700 p-2 bg-zinc-100 rounded">{ user?.email}</p>
+              <p className="text-xs font-semibold text-zinc-700 p-2 bg-zinc-100 rounded">{user?.email}</p>
+              
+              <UpdateUserModal/>
       </Card>
     </div>
   );
